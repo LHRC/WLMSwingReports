719a748c2dbcc0b36d6a35c5ba91339050b97f15
mdonovan3 <martindonovan3@gmail.com>
2020-04-19 20:05:22 -0400

    added menu

--- src/wlmswingreports/MainFrame.java
+++ src/wlmswingreports/MainFrame.java
@@ -14,6 +14,9 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -34,19 +37,26 @@ public class MainFrame extends JFrame
   public MainFrame()
   {
     super();
-    //setJMenuBar(new MainMenuBar());
+    JMenuBar mb = new JMenuBar();
+    JMenu jm = new JMenu("Menu 1");
+    JMenuItem jmi = new JMenuItem("item1");
+    jm.add(jmi);
+    mb.add(jm);
+    setJMenuBar(mb);
     content = (JPanel) getContentPane();
     String space = "                         ";
     String cr = "\u00a9";
     setTitle("Swing Reports Utility");
-    setSize(800,600);
+    setSize(1600,900);
     content.setLayout(new GridLayout(1,1) );
     JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+    this.add(new JMenuBar());
     mainPane.setDividerLocation(100);
     mainPane.setBackground(Color.BLACK);
     //MainMenuTree mmt = new MainMenuTree(ApplicationData.getMenuTreeModel(), mainPane);
     mainPane.setLeftComponent(new JScrollPane());
     JDesktopPane pane = new JDesktopPane();
+
     pane.setBackground(Color.BLACK);
     mainPane.setRightComponent(pane);
     mainPane.setVisible(true);
